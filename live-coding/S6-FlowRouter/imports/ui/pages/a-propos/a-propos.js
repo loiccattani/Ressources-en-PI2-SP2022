84b205f24f8ccf8c3bfbd6f21dbfb9f81797a814
Loïc Cattani <loic@cattani.co>
2022-04-06 11:47:40 +0200

    Utilisation de Session.get/set

--- live-coding/S6-FlowRouter/imports/ui/pages/a-propos/a-propos.js
+++ live-coding/S6-FlowRouter/imports/ui/pages/a-propos/a-propos.js
@@ -1,7 +1,14 @@
+import { Session } from 'meteor/session';
 import { Template } from 'meteor/templating';
 import '../../components/nav/nav';
 import './a-propos.html';
 
 Template.aPropos.onCreated(function () {
 
+});
+
+Template.aPropos.helpers({
+    counter() {
+        return Session.get('counter');
+    },
 });
--- live-coding/S6-FlowRouter/imports/ui/pages/home/home.js
+++ live-coding/S6-FlowRouter/imports/ui/pages/home/home.js
@@ -1,3 +1,4 @@
+import { Session } from 'meteor/session';
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import '../../components/nav/nav';
@@ -5,18 +6,21 @@ import './home.html';
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
-  this.counter = new ReactiveVar(0);
+
+  if (Session.get('counter') === undefined) {
+      Session.set('counter', 0);
+  }
 });
 
 Template.hello.helpers({
   counter() {
-    return Template.instance().counter.get();
+    return Session.get('counter');
   },
 });
 
 Template.hello.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+    Session.set('counter', Session.get('counter') + 1);
   },
 });
